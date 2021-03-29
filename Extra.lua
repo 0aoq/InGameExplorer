12957a8adc73e2fa5286b4f3dc5c897013899df4
0aoq <73413330+0aoq@users.noreply.github.com>
2021-03-29 17:30:29 -0400

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -1,6 +1,8 @@
 -- Gui to Lua
 -- Version: 3.2
 
+local modWeapons = false
+
 -- Instances:
 
 local ExplorerExtra = Instance.new("ScreenGui")
@@ -18,19 +20,14 @@ local TextLabel_3 = Instance.new("TextLabel")
 local TextButton_3 = Instance.new("TextButton")
 local TextLabel_4 = Instance.new("TextLabel")
 local UICorner_4 = Instance.new("UICorner")
-local TextButton_4 = Instance.new("TextButton")
 local TextLabel_5 = Instance.new("TextLabel")
-local UICorner_5 = Instance.new("UICorner")
+local TextButton_4 = Instance.new("TextButton")
 local TextLabel_6 = Instance.new("TextLabel")
+local UICorner_5 = Instance.new("UICorner")
+local UICorner_6 = Instance.new("UICorner")
 local TextButton_5 = Instance.new("TextButton")
 local TextLabel_7 = Instance.new("TextLabel")
-local UICorner_6 = Instance.new("UICorner")
 local UICorner_7 = Instance.new("UICorner")
-local TextButton_6 = Instance.new("TextButton")
-local TextLabel_8 = Instance.new("TextLabel")
-local UICorner_8 = Instance.new("UICorner")
-
-local Player = game.Players.LocalPlayer
 
 --Properties:
 
@@ -127,11 +124,11 @@ TextLabel_3.TextScaled = true
 TextLabel_3.TextSize = 14.000
 TextLabel_3.TextWrapped = true
 
-TextButton_3.Parent = PrisonLife
+TextButton_3.Parent = Options
 TextButton_3.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
 TextButton_3.BorderSizePixel = 0
-TextButton_3.Position = UDim2.new(0.200031579, 0, 0.58666271, 0)
-TextButton_3.Size = UDim2.new(0.595039964, 0, 0.0776907876, 0)
+TextButton_3.Position = UDim2.new(0.0853001848, 0, 0.851444006, 0)
+TextButton_3.Size = UDim2.new(0.168225497, 0, 0.102839477, 0)
 TextButton_3.Font = Enum.Font.SourceSans
 TextButton_3.Text = ""
 TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -145,7 +142,7 @@ TextLabel_4.BackgroundTransparency = 1.000
 TextLabel_4.Position = UDim2.new(0.0898997858, 0, 0.203037545, 0)
 TextLabel_4.Size = UDim2.new(0.822845757, 0, 0.553756773, 0)
 TextLabel_4.Font = Enum.Font.Ubuntu
-TextLabel_4.Text = "Fix Inventory"
+TextLabel_4.Text = "X"
 TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_4.TextScaled = true
 TextLabel_4.TextSize = 14.000
@@ -154,11 +151,23 @@ TextLabel_4.TextWrapped = true
 UICorner_4.CornerRadius = UDim.new(0.200000003, 0)
 UICorner_4.Parent = TextButton_3
 
+TextLabel_5.Parent = Options
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.Position = UDim2.new(0.0860756114, 0, 0.0465482473, 0)
+TextLabel_5.Size = UDim2.new(0.822845757, 0, 0.0792390555, 0)
+TextLabel_5.Font = Enum.Font.Ubuntu
+TextLabel_5.Text = "Explorer Extra"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 14.000
+TextLabel_5.TextWrapped = true
+
 TextButton_4.Parent = Options
 TextButton_4.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
 TextButton_4.BorderSizePixel = 0
-TextButton_4.Position = UDim2.new(0.0853001848, 0, 0.853464067, 0)
-TextButton_4.Size = UDim2.new(0.168225497, 0, 0.100819759, 0)
+TextButton_4.Position = UDim2.new(0.196207166, 0, 0.273684263, 0)
+TextButton_4.Size = UDim2.new(0.595000029, 0, 0.0780000016, 0)
 TextButton_4.Font = Enum.Font.SourceSans
 TextButton_4.Text = ""
 TextButton_4.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -166,38 +175,30 @@ TextButton_4.TextScaled = true
 TextButton_4.TextSize = 14.000
 TextButton_4.TextWrapped = true
 
-TextLabel_5.Parent = TextButton_4
-TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_5.BackgroundTransparency = 1.000
-TextLabel_5.Position = UDim2.new(0.0898997858, 0, 0.203037545, 0)
-TextLabel_5.Size = UDim2.new(0.822845757, 0, 0.553756773, 0)
-TextLabel_5.Font = Enum.Font.Ubuntu
-TextLabel_5.Text = "X"
-TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_5.TextScaled = true
-TextLabel_5.TextSize = 14.000
-TextLabel_5.TextWrapped = true
-
-UICorner_5.CornerRadius = UDim.new(0.200000003, 0)
-UICorner_5.Parent = TextButton_4
-
-TextLabel_6.Parent = Options
+TextLabel_6.Parent = TextButton_4
 TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_6.BackgroundTransparency = 1.000
-TextLabel_6.Position = UDim2.new(0.0860756114, 0, 0.0465482473, 0)
-TextLabel_6.Size = UDim2.new(0.822845757, 0, 0.0792390555, 0)
+TextLabel_6.Position = UDim2.new(0.0898997858, 0, 0.203037545, 0)
+TextLabel_6.Size = UDim2.new(0.822845757, 0, 0.553756773, 0)
 TextLabel_6.Font = Enum.Font.Ubuntu
-TextLabel_6.Text = "Explorer Extra"
+TextLabel_6.Text = "ESP"
 TextLabel_6.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_6.TextScaled = true
 TextLabel_6.TextSize = 14.000
 TextLabel_6.TextWrapped = true
 
-TextButton_5.Parent = Options
+UICorner_5.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_5.Parent = TextButton_4
+
+UICorner_6.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_6.Parent = Main
+
+TextButton_5.Parent = ExplorerExtra
 TextButton_5.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
 TextButton_5.BorderSizePixel = 0
-TextButton_5.Position = UDim2.new(0.196207166, 0, 0.273684263, 0)
-TextButton_5.Size = UDim2.new(0.595000029, 0, 0.0780000016, 0)
+TextButton_5.Position = UDim2.new(0.913664877, 0, 0.950649023, 0)
+TextButton_5.Size = UDim2.new(0.0792919695, 0, 0.0365006216, 0)
+TextButton_5.Visible = false
 TextButton_5.Font = Enum.Font.SourceSans
 TextButton_5.Text = ""
 TextButton_5.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -211,49 +212,18 @@ TextLabel_7.BackgroundTransparency = 1.000
 TextLabel_7.Position = UDim2.new(0.0898997858, 0, 0.203037545, 0)
 TextLabel_7.Size = UDim2.new(0.822845757, 0, 0.553756773, 0)
 TextLabel_7.Font = Enum.Font.Ubuntu
-TextLabel_7.Text = "ESP"
+TextLabel_7.Text = "Explorer Extra"
 TextLabel_7.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_7.TextScaled = true
 TextLabel_7.TextSize = 14.000
 TextLabel_7.TextWrapped = true
 
-UICorner_6.CornerRadius = UDim.new(0.200000003, 0)
-UICorner_6.Parent = TextButton_5
-
-UICorner_7.CornerRadius = UDim.new(0.100000001, 0)
-UICorner_7.Parent = Main
-
-TextButton_6.Parent = ExplorerExtra
-TextButton_6.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
-TextButton_6.BorderSizePixel = 0
-TextButton_6.Position = UDim2.new(0.913664877, 0, 0.950649023, 0)
-TextButton_6.Size = UDim2.new(0.0792919695, 0, 0.0365006216, 0)
-TextButton_6.Visible = false
-TextButton_6.Font = Enum.Font.SourceSans
-TextButton_6.Text = ""
-TextButton_6.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_6.TextScaled = true
-TextButton_6.TextSize = 14.000
-TextButton_6.TextWrapped = true
-
-TextLabel_8.Parent = TextButton_6
-TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_8.BackgroundTransparency = 1.000
-TextLabel_8.Position = UDim2.new(0.0898997858, 0, 0.203037545, 0)
-TextLabel_8.Size = UDim2.new(0.822845757, 0, 0.553756773, 0)
-TextLabel_8.Font = Enum.Font.Ubuntu
-TextLabel_8.Text = "Explorer Extra"
-TextLabel_8.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_8.TextScaled = true
-TextLabel_8.TextSize = 14.000
-TextLabel_8.TextWrapped = true
-
-UICorner_8.CornerRadius = UDim.new(0.200000003, 0)
-UICorner_8.Parent = TextButton_6
+UICorner_7.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_7.Parent = TextButton_5
 
 -- Scripts:
 
-local function GWZPA_fake_script() -- TextButton.Expand 
+local function FBLWR_fake_script() -- TextButton.Expand 
 	local script = Instance.new('LocalScript', TextButton)
 
 	local DefaultColor = script.Parent.BackgroundColor3
@@ -267,9 +237,96 @@ local function GWZPA_fake_script() -- TextButton.Expand
 	script.Parent.MouseLeave:Connect(function()
 		TweenService:Create(script.Parent, TweenInfo.new(0.2), {BackgroundColor3 = DefaultColor}):Play()
 	end)
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		local Player = game:GetService("Players").LocalPlayer
+		local Mouse = Player:GetMouse()
+	
+		local flying = false
+		local enabled = true
+	
+		Mouse.KeyDown:connect(function(key)
+			if key == "f" then
+				if enabled then
+					repeat wait() until Player and Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") and Player.Character:FindFirstChild("Humanoid")
+					repeat wait() until Mouse
+					flyspeed = 1
+	
+					local T = Player.Character.HumanoidRootPart
+					local CONTROL = {F = 0, B = 0, L = 0, R = 0}
+					local lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+					local SPEED = 30
+	
+					local function fly()
+						flying = true
+						local BG = Instance.new("BodyGyro", T)
+						local BV = Instance.new("BodyVelocity", T)
+						BG.P = 9e4
+						BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+						BG.cframe = T.CFrame
+						BV.velocity = Vector3.new(0, 0.1, 0)
+						BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+						spawn(function()
+							repeat wait()
+								Player.Character.Humanoid.PlatformStand = true
+								if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 then
+									SPEED = 50
+								elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0) and SPEED ~= 0 then
+									SPEED = 0
+								end
+								if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 then
+									BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+									lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+								elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and SPEED ~= 0 then
+									BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+								else
+									BV.velocity = Vector3.new(0, 0.1, 0)
+								end
+								BG.cframe = workspace.CurrentCamera.CoordinateFrame
+							until not flying
+							CONTROL = {F = 0, B = 0, L = 0, R = 0}
+							lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+							SPEED = 0
+							BG:destroy()
+							BV:destroy()
+							Player.Character.Humanoid.PlatformStand = false
+						end)
+					end
+					Mouse.KeyDown:connect(function(KEY)
+						if KEY:lower() == "w" then
+							CONTROL.F = flyspeed
+						elseif KEY:lower() == "s" then
+							CONTROL.B = -flyspeed
+						elseif KEY:lower() == "a" then
+							CONTROL.L = -flyspeed
+						elseif KEY:lower() == "d" then
+							CONTROL.R = flyspeed
+						end
+					end)
+					Mouse.KeyUp:connect(function(KEY)
+						if KEY:lower() == "w" then
+							CONTROL.F = 0
+						elseif KEY:lower() == "s" then
+							CONTROL.B = 0
+						elseif KEY:lower() == "a" then
+							CONTROL.L = 0
+						elseif KEY:lower() == "d" then
+							CONTROL.R = 0
+						end
+					end)
+					enabled = false
+					fly()
+				else 
+					flying = false
+					Player.Character.Humanoid.PlatformStand = false
+					enabled = true
+				end 
+			end 
+		end)
+	end)
 end
-coroutine.wrap(GWZPA_fake_script)()
-local function JZKZ_fake_script() -- TextButton_2.Expand 
+coroutine.wrap(FBLWR_fake_script)()
+local function IKOP_fake_script() -- TextButton_2.Expand 
 	local script = Instance.new('LocalScript', TextButton_2)
 
 	local DefaultColor = script.Parent.BackgroundColor3
@@ -291,48 +348,12 @@ local function JZKZ_fake_script() -- TextButton_2.Expand
 		wait(0.1)
 		workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS:findFirstChild('M4A1', true).ITEMPICKUP)
 	
-		local Shotgun = require(Player.Backpack:FindFirstChild("Remington 870").GunStates)
-	
-		Shotgun.MaxAmmo = math.huge
-		Shotgun.Bullets = 75
-		Shotgun.AutoFire = true
-	
-		local AK47 = require(Player.Backpack:FindFirstChild("AK-47").GunStates)
-	
-		AK47.MaxAmmo = math.huge
-		AK47.Bullets = 75
-		AK47.AutoFire = true
-	
-		local M4A1 = require(Player.Backpack:FindFirstChild("M4A1").GunStates)
-	
-		M4A1.MaxAmmo = math.huge
-		M4A1.Bullets = 75
-		M4A1.AutoFire = true
+		modWeapons = true
 	end)
 end
-coroutine.wrap(JZKZ_fake_script)()
-local function XDZV_fake_script() -- TextButton_3.Expand 
+coroutine.wrap(IKOP_fake_script)()
+local function FNHGYJ_fake_script() -- TextButton_3.Expand 
 	local script = Instance.new('LocalScript', TextButton_3)
-
-	local DefaultColor = script.Parent.BackgroundColor3
-	
-	local TweenService = game:GetService("TweenService")
-	
-	script.Parent.MouseEnter:Connect(function()
-		TweenService:Create(script.Parent, TweenInfo.new(0.2), {BackgroundColor3 = Color3.fromRGB(2, 183, 87)}):Play()
-	end)
-	
-	script.Parent.MouseLeave:Connect(function()
-		TweenService:Create(script.Parent, TweenInfo.new(0.2), {BackgroundColor3 = DefaultColor}):Play()
-	end)
-	
-	script.Parent.MouseButton1Click:Connect(function()
-		Player.Backpack:ClearAllChildren()
-	end)
-end
-coroutine.wrap(XDZV_fake_script)()
-local function XMDU_fake_script() -- TextButton_4.Expand 
-	local script = Instance.new('LocalScript', TextButton_4)
 
 	local DefaultColor = script.Parent.BackgroundColor3
 	
@@ -351,9 +372,9 @@ local function XMDU_fake_script() -- TextButton_4.Expand
 		script.Parent.Parent.Parent.Parent.TextButton.Visible = true
 	end)
 end
-coroutine.wrap(XMDU_fake_script)()
-local function NDHIGV_fake_script() -- TextButton_5.Expand 
-	local script = Instance.new('LocalScript', TextButton_5)
+coroutine.wrap(FNHGYJ_fake_script)()
+local function ORTKME_fake_script() -- TextButton_4.Expand 
+	local script = Instance.new('LocalScript', TextButton_4)
 
 	local DefaultColor = script.Parent.BackgroundColor3
 	
@@ -367,9 +388,9 @@ local function NDHIGV_fake_script() -- TextButton_5.Expand
 		TweenService:Create(script.Parent, TweenInfo.new(0.2), {BackgroundColor3 = DefaultColor}):Play()
 	end)
 end
-coroutine.wrap(NDHIGV_fake_script)()
-local function LCZFDRX_fake_script() -- TextButton_6.Expand 
-	local script = Instance.new('LocalScript', TextButton_6)
+coroutine.wrap(ORTKME_fake_script)()
+local function WPSMS_fake_script() -- TextButton_5.Expand 
+	local script = Instance.new('LocalScript', TextButton_5)
 
 	local DefaultColor = script.Parent.BackgroundColor3
 	
@@ -388,4 +409,23 @@ local function LCZFDRX_fake_script() -- TextButton_6.Expand
 		script.Parent.Visible = false
 	end)
 end
-coroutine.wrap(LCZFDRX_fake_script)()
+coroutine.wrap(WPSMS_fake_script)()
+
+
+while true do
+	if modWeapons then
+		for i, v in next, debug.getregistry() do
+			if type(v) == 'table' then
+				if v.Bullets then
+					v.AutoFire = true
+					v.FireRate = 0
+					v.Bullets = 5
+					v.Range = math.huge
+					v.MaxAmmo = math.huge
+					v.CurrentAmmo = math.huge
+					v.StoredAmmo = math.huge
+				end
+			end
+		end
+	end
+end
